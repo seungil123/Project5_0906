7f7b4d9a85f1bfdb8822e6cc0d2b5cc356331788
ohriyang <saimck0701@gmail.com>
2024-09-06 15:59:05 +0900

    대출일자 업데이트

--- update.py
+++ update.py
@@ -3,3 +3,6 @@ from common import books
 def update_book(book_id, borrow_date=None, is_returned=None):
       if book_id not in books:
         print("책을 찾을 수 없습니다.")
+      else:
+        if borrow_date is not None:
+            books[book_id]['borrow_date'] = borrow_date
