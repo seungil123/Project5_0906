6e3e9facececbd45145aee5c2c020df30da355df
ggabaegii <117845415+ggabaegii@users.noreply.github.com>
2024-09-06 16:34:52 +0900

    readBooks 수정

--- readBooks.py
+++ readBooks.py
@@ -1,10 +1,10 @@
-from books_records import books
+from books import books_records
 
 
 def read_books():
     print("도서 목록 조회")
     print("="*50)
-    for book in books:
+    for book in books_records:
         print(f"ID: {book['id']}, 제목: {book['title']}, 작가: {book['author']}, "
               f"상태: {book['status']}, 대출일: {book['date']}")
     print("="*50)
