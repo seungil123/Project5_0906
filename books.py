7d7ebe410b4542405af82965e32b2372ff389768
cho <ill27451@gmail.com>
2024-09-06 16:24:21 +0900

    도서목록 이동

--- books.py
+++ books.py
@@ -1,1 +1,4 @@
-books_record = []
+# 도서목록
+books_record = [{"id": "001", "title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "date": "2024-09-06", "status": "대출 중"},
+    {"id": "002", "title": "1984", "author": "George Orwell", "date": "2024-08-20", "status": "반납 완료"},
+    {"id": "003", "title": "To Kill a Mockingbird", "author": "Harper Lee", "date": "2024-09-01", "status": "대출 중"}]
