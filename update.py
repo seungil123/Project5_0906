27656e15f412ecb8b1018b90a7df1ca2e9a53a93
ohriyang <saimck0701@gmail.com>
2024-09-06 16:19:38 +0900

    코드 수정 + 도서삭제

--- update.py
+++ update.py
@@ -1,15 +1,5 @@
-from common import books
-
-def update_book(book_id, borrow_date=None, is_returned=None):
-      if book_id not in books:
-        print("책을 찾을 수 없습니다.")
-      else:
-        if borrow_date is not None:
-            books[book_id]['borrow_date'] = borrow_date
-        if is_returned is not None:
-            books[book_id]['is_returned'] = is_returned
-        print(f"책 ID {book_id}의 정보가 업데이트되었습니다.")
-        
-if __name__ == "__main__":
-    # 테스트 용도
-    update_book('1', '2024-09-05', True)
+def delete_book(books, book_id):
+    for book in books:
+        if book['id'] == book_id:
+            books.remove(book)
+            break
