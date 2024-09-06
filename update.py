20758592e309ca5db2bce9767f491f2c6b4825a7
ohriyang <saimck0701@gmail.com>
2024-09-06 16:01:13 +0900

    테스트용 책 정보

--- update.py
+++ update.py
@@ -9,3 +9,7 @@ def update_book(book_id, borrow_date=None, is_returned=None):
         if is_returned is not None:
             books[book_id]['is_returned'] = is_returned
         print(f"책 ID {book_id}의 정보가 업데이트되었습니다.")
+        
+if __name__ == "__main__":
+    # 테스트 용도
+    update_book('1', '2024-09-05', True)
