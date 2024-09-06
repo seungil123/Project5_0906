1ac5bc4ededd795c615629da40ec3358d43a216e
ohriyang <saimck0701@gmail.com>
2024-09-06 16:32:21 +0900

    도서목록 추가

--- update.py
+++ update.py
@@ -1,9 +1,6 @@
+import books
 def delete_book(books, book_id):
     for book in books:
         if book['id'] == book_id:
             books.remove(book)
-            break
-        
-def read_books(books):
-    for book in books:
-        print(f"ID: {book['id']}, 제목: {book['title']}, 작가: {book['author']}, 대출일자: {book['date']}, 상태: {book['status']}")
+          
