01c9053907932f1831da1e930057efb843715698
ohriyang <saimck0701@gmail.com>
2024-09-06 16:34:46 +0900

    수정

--- update.py
+++ update.py
@@ -1,6 +1,9 @@
-import books
-def delete_book(books, book_id):
+from books import books_record
+def update_book(books, book_id, new_date, new_status):
     for book in books:
         if book['id'] == book_id:
-            books.remove(book)
+            book['date'] = new_date
+            book['status'] = new_status
+            break
+
           
